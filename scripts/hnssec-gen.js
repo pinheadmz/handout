23a403e92fd56d9f17e1380e181dfedf2a4e89e9
Matthew Zipkin <pinheadmz@gmail.com>
2020-06-25 16:16:22 -0400

    create SSL key and cert with bcrypto + switch webserver to https

--- scripts/hnssec-gen.js
+++ scripts/hnssec-gen.js
@@ -4,14 +4,156 @@
 const argv = process.argv;
 if (argv.length !== 4)
   throw new Error('usage: node hnssec-gen.js <NAME> <HOST-IP>');
-const name = argv[2];
+let name = argv[2];
 const host = argv[3];
+
+if (name.slice(-1) === '.')
+  name = name.slice(0, -1);
 
 const fs = require('fs');
 const Path = require('path');
 const {dnssec} = require('bns');
 const {RSASHA256} = dnssec.algs;
 const {ZONE, KSK} = dnssec.keyFlags;
+const {rsa, SHA256, encoding} = require('bcrypto');
+const {x509, pem} = encoding;
+
+console.log('\nGenerating SSL key and self-signed certificate...');
+
+// Create key pair and get JSON for pubkey
+const priv = rsa.privateKeyGenerate(2048);
+const pub = rsa.publicKeyCreate(priv);
+const pubJSON = rsa.publicKeyExport(pub);
+
+// Timestamps and serial number
+// Use *yesterday* for start time to avoid UTC/timezone conflict
+const date = new Date();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+if (day > 1) {
+  date.setDate(day - 1);
+} else {
+  date.setMonth((month + 11) % 12);
+  date.setDate(30);
+}
+
+const serial =
+  String(date.getFullYear()) +
+  ('0' + String(month)).slice(-2) +
+  ('0' + String(day)).slice(-2) +
+  '00';
+const notBefore = date.toISOString().split('.')[0] + 'Z';
+date.setMonth(date.getMonth() + 3);
+const notAfter = date.toISOString().split('.')[0] + 'Z';
+
+// hex-encode IP address
+let ipaddr = '';
+const bytes = host.split('.');
+for (const byte of bytes)
+  ipaddr += Buffer.from([parseInt(byte)]).toString('hex');
+
+// Basic details, leave out optional and more complex stuff
+const tbsJSON = {
+  version: 2,
+  serialNumber: serial,
+  signature: {
+    algorithm: 'RSASHA256',
+    parameters: {
+      type: 'NULL',
+      node: null
+    }
+  },
+  issuer: [],
+  validity: {
+    notBefore: { type: 'UTCTime', node: notBefore },
+    notAfter: { type: 'UTCTime', node: notAfter }
+  },
+  subject: [],
+  subjectPublicKeyInfo: {
+    algorithm: {
+      algorithm: 'RSAPublicKey',
+      parameters: {
+        type: 'NULL',
+        node: null
+      }
+    },
+    publicKey: {
+      modulus: pubJSON.n,
+      publicExponent: pubJSON.e
+    }
+  },
+  extensions: [
+    {
+      extnID: 'SubjectAltName',
+      critical: false,
+      extnValue: [
+        { type: 'DNSName', node: name },
+        { type: 'DNSName', node: `*.${name}` },
+        { type: 'IPAddress', node: ipaddr }
+      ]
+    },
+    {
+      extnID: 'BasicConstraints',
+      critical: false,
+      extnValue: {cA: false, pathLenConstraint: 0}
+    },
+    {
+      extnID: 'KeyUsage',
+      critical: false,
+      extnValue: [
+        'digitalSignature',
+        'nonRepudiation',
+        'keyEncipherment',
+        'dataEncipherment'
+      ]
+    }
+  ]
+};
+
+// Create to-be-signed certificate object
+const tbs = x509.TBSCertificate.fromJSON(tbsJSON);
+
+// Use helper functions for the complicated details
+tbs.issuer = x509.Entity.fromJSON({
+  COMMONNAME: name
+});
+tbs.subject = x509.Entity.fromJSON({
+  COMMONNAME: name
+});
+
+// Serialize
+const msg = SHA256.digest(tbs.encode());
+
+// Sign
+const sig = rsa.sign('SHA256', msg, priv);
+
+// Complete
+const cert = new x509.Certificate();
+cert.tbsCertificate = tbs;
+cert.signatureAlgorithm.fromJSON({
+  algorithm: 'RSASHA256',
+  parameters: {
+    type: 'NULL',
+    node: null
+  }
+});
+cert.signature.fromJSON({bits: sig.length * 8, value: sig.toString('hex')});
+
+// Output SSL
+fs.writeFileSync(
+  Path.join(__dirname, '..', 'conf', 'ssl', `${name}.crt`),
+  cert.toPEM()
+);
+
+fs.writeFileSync(
+  Path.join(__dirname, '..', 'conf', 'ssl', `${name}.key`),
+  pem.toPEM(priv, 'RSA PRIVATE KEY')
+);
+
+// DNSSEC
+name += '.';
+
+console.log('\nGenerating DNSSEC keys...');
 
 const kpriv = dnssec.createPrivate(RSASHA256, 2048);
 const kkey = dnssec.makeKey(name, RSASHA256, kpriv, ZONE | KSK);
