eb44ea524547e4bfbbd80c549fdf5cc478766b4c
Matthew Zipkin <pinheadmz@gmail.com>
2020-07-08 10:42:42 -0400

    favicon

--- app/edit.js
+++ app/edit.js
@@ -14,6 +14,9 @@ class Edit {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/milligram.css">
+  <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
 
 <title>${this.name} . Proof Of Concept</title>
 </head>
--- app/index.js
+++ app/index.js
@@ -21,6 +21,9 @@ class Index {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/milligram.css">
+  <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
 
 <title>Proof Of Concept</title>
 </head>
